8db759a2fc2413c9201fdbf9b489968fdb33944c
jkimel <jkimel@mail.austral.edu.ar>
2025-03-31 12:05:47 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,16 @@
 def change():
     expense = 23.75
     money = 100
+    restante = money - expense
+    print('Ingresar gasto:')
+    print(expense)
+    print('Dinero recibido')
+    print(money)
+    print("")
+    print('Vuelto')
+    print("")
+    print('Pesos:')
+    print(int(restante))
+    print('Centavos:')
+    print(int((restante - int(restante))*100))
+change()
